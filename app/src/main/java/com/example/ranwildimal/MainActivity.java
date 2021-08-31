05e28adbf92b65f0781077d932cca3556089053a
nphtai <huutaimenst10@gmail.com>
2021-08-31 18:36:04 +0700

    Sidebar lô hốp (dm android)
    Éo hiểu sao máy thằng Minh có hiện mà máy t ko hiện ?? :D ??

--- app/src/main/java/com/example/ranwildimal/MainActivity.java
+++ app/src/main/java/com/example/ranwildimal/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.ranwildimal;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+
+import com.google.android.material.navigation.NavigationView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawer;
+    NavigationView sidebar;
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawer = findViewById(R.id.drawer_layout);
+        sidebar = findViewById(R.id.main_sidebar);
+        toolbar = findViewById(R.id.main_toolbar);
+
+//        setSupportActionBar(toolbar);
+
+        sidebar.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.sidebar_open, R.string.sidebar_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        sidebar.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+        return true;
     }
 }
