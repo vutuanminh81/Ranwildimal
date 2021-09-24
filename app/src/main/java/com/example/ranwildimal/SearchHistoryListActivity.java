de277d897a8610f1dabd0f32f4cc503c1acb80ee
nphtai <huutaimenst10@gmail.com>
2021-09-24 17:18:14 +0700

    Responsive trang main, xong search và encounter

---
+++ app/src/main/java/com/example/ranwildimal/SearchHistoryListActivity.java
@@ -0,0 +1,14 @@
+package com.example.ranwildimal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchHistoryListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_history_list);
+    }
+}
