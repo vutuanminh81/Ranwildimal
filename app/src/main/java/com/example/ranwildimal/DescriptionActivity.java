8c78054c5fa4ea25b49500535e20a5277143b0c4
nphtai <huutaimenst10@gmail.com>
2021-10-20 16:02:05 +0700

    Thêm trang description

---
+++ app/src/main/java/com/example/ranwildimal/DescriptionActivity.java
@@ -0,0 +1,39 @@
+package com.example.ranwildimal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+public class DescriptionActivity extends AppCompatActivity {
+
+    Toolbar description_toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_description);
+        description_toolbar = findViewById(R.id.search_toolbar);
+        //Customize status bar
+        statusBarColor();
+        //Customize toolbar
+        setSupportActionBar(description_toolbar);
+        getSupportActionBar().setTitle(null);
+    }
+
+    private void statusBarColor(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            getWindow().setStatusBarColor(getResources().getColor(R.color.main_color,this.getTheme()));
+        }else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            getWindow().setStatusBarColor(getResources().getColor(R.color.main_color));
+        }
+    }
+
+    public void HomeIntent(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        this.startActivity(intent);
+    }
+}
