d2d66772862ef4c6b0ee9bc79767cfe2f0c045e7
okbayby22 <okbayby22@gmail.com>
2022-04-07 16:03:03 +0700

    Build class model

--- app/src/main/java/com/example/ranwildimal/database/DatabaseAccess.java
+++ app/src/main/java/com/example/ranwildimal/database/DatabaseAccess.java
@@ -8,8 +8,11 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Bitmap;
 
 import com.example.ranwildimal.model.Example;
+import com.example.ranwildimal.model.ExampleModel;
 import com.example.ranwildimal.model.Word;
+import com.example.ranwildimal.model.WordModel;
 import com.example.ranwildimal.model.Word_Description;
+import com.example.ranwildimal.model.Word_DescriptionModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +23,11 @@ public class DatabaseAccess {
     private SQLiteDatabase db;
     private static DatabaseAccess instance;
     Cursor c = null;
+
+    WordModel wordModel;
+    ExampleModel exampleModel;
+    Word_DescriptionModel wordDescriptionModel;
+
 
     /**
      * Private constructor so that object creation from outside the class
@@ -57,204 +65,67 @@ public class DatabaseAccess {
     }
 
     public ArrayList<Word> getWord(){
-        c = db.rawQuery("select * from Word", new String[]{});
-        StringBuffer buffer = new StringBuffer();
-        ArrayList<Word> list = new ArrayList<>();
-        while(c.moveToNext()){
-            int id = c.getInt(0);
-            String word = c.getString(1);
-            int language = c.getInt(2);
-            int word_des = c.getInt(3);
-            int type = c.getInt(4);
-            Word new_word = new Word(id,word,language,word_des,type,1);
-            list.add(new_word);
-        }
-        c.close();
-        return list;
+        wordModel = new WordModel();
+        return wordModel.getWord(c,db);
     }
 
     public ArrayList<Example> getExample(){
-        c = db.rawQuery("select * from Example", new String[]{});
-        StringBuffer buffer = new StringBuffer();
-        ArrayList<Example> list = new ArrayList<>();
-        while(c.moveToNext()){
-            int id = c.getInt(0);
-            String example = c.getString(1);
-            int word_id = c.getInt(2);
-
-            Example new_ex = new Example(id,example,word_id);
-            list.add(new_ex);
-        }
-        c.close();
-        return list;
+        exampleModel = new ExampleModel();
+        return exampleModel.getExample(c,db);
     }
 
     public ArrayList<Word_Description> getWordDes(){
-        c = db.rawQuery("select * from Word_Description", new String[]{});
-        StringBuffer buffer = new StringBuffer();
-        ArrayList<Word_Description> list = new ArrayList<>();
-        while(c.moveToNext()){
-            int id = c.getInt(0);
-            String pronounce = c.getString(1);
-            String video = c.getString(2);
-            String image = c.getString(3);
-            int scan = c.getInt(4);
-            int status = 1;
-            Word_Description new_word = new Word_Description(id,pronounce,video,image,1);
-            list.add(new_word);
-        }
-        c.close();
-        return list;
+        wordDescriptionModel = new Word_DescriptionModel();
+        return wordDescriptionModel.getWordDes(c,db);
     }
 
     public ArrayList<Example> getExampleListByWord(String wordId){
-        c = db.rawQuery("select * from Example where Word_Id = "+wordId, new String[]{});
-        StringBuffer buffer = new StringBuffer();
-        ArrayList<Example> list = new ArrayList<>();
-        while(c.moveToNext()){
-            int id = c.getInt(0);
-            String example = c.getString(1);
-            int word_id = c.getInt(2);
-            Example new_example = new Example(id,example,word_id);
-            list.add(new_example);
-        }
-        c.close();
-        return list;
+        exampleModel = new ExampleModel();
+        return exampleModel.getExampleListByWord(wordId,c,db);
     }
 
     public Word getOneWordById(String wordDesId, String locale){
-        int langId = 1;
-        if(locale.equals("vi") || locale.equalsIgnoreCase("vi_VN")){
-            langId = 1;
-        }else if(locale.equals("en") || locale.equalsIgnoreCase("en_US")){
-            langId = 2;
-        }else if(locale.equals("ja") || locale.equalsIgnoreCase("ja_JP")){
-            langId = 3;
-        }
-        c = db.rawQuery("select * from Word where Word_Des_Id = " + wordDesId + " and Language_Id = " + langId, new String[]{});
-        StringBuffer buffer = new StringBuffer();
-        ArrayList<Word> list = new ArrayList<>();
-        while(c.moveToNext()){
-            int w_id = c.getInt(0);
-            String word = c.getString(1);
-            int language = c.getInt(2);
-            int word_des = c.getInt(3);
-            int type = c.getInt(4);
-            Word new_word = new Word(w_id,word,language,word_des,type,1);
-            return new_word;
-        }
-        c.close();
-        return null;
+        wordModel = new WordModel();
+        return wordModel.getOneWordById(wordDesId,locale,c,db);
     }
 
     public Word getOneWordByLanguage(String wordDesId, String languageId){
-        c = db.rawQuery("select * from Word where Word_Des_Id = " + wordDesId + " and Language_Id = " + languageId, new String[]{});
-        StringBuffer buffer = new StringBuffer();
-        ArrayList<Word> list = new ArrayList<>();
-        while(c.moveToNext()){
-            int id = c.getInt(0);
-            String word = c.getString(1);
-            int language = c.getInt(2);
-            int word_des = c.getInt(3);
-            int type = c.getInt(4);
-            Word new_word = new Word(id,word,language,word_des,type,1);
-            return new_word;
-        }
-        c.close();
-        return null;
+        wordModel = new WordModel();
+        return wordModel.getOneWordByLanguage(wordDesId, languageId, c, db);
     }
-
 
     public ArrayList<Word> searchWord(String search, String locale){
-        int langId = 1;
-        if(locale.equals("vi")){
-            langId = 1;
-        }else if(locale.equals("en")){
-            langId = 2;
-        }else if(locale.equals("ja")){
-            langId = 3;
-        }
-        c = db.rawQuery("select * from Word where Word like '%"+search+"%' and Language_Id = "+langId, new String[]{});
-        StringBuffer buffer = new StringBuffer();
-        ArrayList<Word> list = new ArrayList<>();
-        while(c.moveToNext()){
-            int id = c.getInt(0);
-            String word = c.getString(1);
-            int language = c.getInt(2);
-            int word_des = c.getInt(3);
-            int type = c.getInt(4);
-            Word new_word = new Word(id,word,language,word_des,type,1);
-            list.add(new_word);
-        }
-        c.close();
-        return list;
-    }
-
-
-    public int searchAnimalbyname(String search){
-        int id = 0;
-        c = db.rawQuery("select * from Word where UPPER(Word) like UPPER('%"+search+"%')and Language_Id = 2", new String[]{});
-        StringBuffer buffer = new StringBuffer();
-        ArrayList<Word> list = new ArrayList<>();
-        while(c.moveToNext()){
-            id = c.getInt(0);
-        }
-        c.close();
-        return id;
+        wordModel = new WordModel();
+        return wordModel.searchWord(search,locale,c,db);
     }
 
     public Word_Description getWordDes(String id){
-        Word_Description word_des = new Word_Description();
-        c = db.rawQuery("select * from Word_Description where Word_Des_Id = "+id, new String[]{});
-        StringBuffer buffer = new StringBuffer();
-        while(c.moveToNext()){
-            word_des.setWord_Des_Id(c.getInt(0));
-            word_des.setWord_Pronounce(c.getString(1));
-            word_des.setWord_Video(c.getString(2));
-            word_des.setWord_Image(c.getString(3));
-        }
-        c.close();
-        return word_des;
+        wordDescriptionModel = new Word_DescriptionModel();
+        return wordDescriptionModel.getWordDes(id,c,db);
     }
 
     public void increaseWordSearch(String id){
-        c = db.rawQuery("update Word_Description set Num_Of_Search = Num_Of_Search + 1  where Word_Des_Id = "+id, null);
-        StringBuffer buffer = new StringBuffer();
-        c.moveToFirst();
-        c.close();
+        wordDescriptionModel = new Word_DescriptionModel();
+        wordDescriptionModel.increaseWordSearch(id,c,db);
     }
 
     public void increaseWordScan(String id){
-        c = db.rawQuery("update Word_Description set Num_Of_Scan = Num_Of_Scan + 1  where Word_Des_Id = "+id, null);
-        StringBuffer buffer = new StringBuffer();
-        c.moveToFirst();
-        c.close();
+        wordDescriptionModel = new Word_DescriptionModel();
+        wordDescriptionModel.increaseWordScan(id,c,db);
     }
 
     public void updateWordDes(Word_Description word){
-        c = db.rawQuery("update Word_Description set Word_Image = ?, Word_Pronounce = ?, Word_Video = ?, Word_Status = 0  where Word_Des_Id = ?", new String[]{word.getWord_Image(),word.getWord_Pronounce(),word.getWord_Video(),String.valueOf(word.getWord_Des_Id())});
-        StringBuffer buffer = new StringBuffer();
-        c.moveToFirst();
-        c.close();
+        wordDescriptionModel = new Word_DescriptionModel();
+        wordDescriptionModel.updateWordDes(word,c,db);
     }
 
     public void updateWord(Word word){
-        c = db.rawQuery("update Word set Word = ?, Word_Des_Id = ?, Word_Status = 1, Word_Type_Id = ?, Language_Id = ? where Word_Id = ?", new String[]{word.getWord(), String.valueOf(word.getWord_Des_Id()), String.valueOf(word.getWord_Type_Id()),String.valueOf(word.getLanguage_Id()), String.valueOf(word.getWord_ID())});
-        StringBuffer buffer = new StringBuffer();
-        c.moveToFirst();
-        c.close();
+        wordModel = new WordModel();
+        wordModel.updateWord(word,c,db);
     }
 
     public int getWordDesIdbyName(String id){
-        String new_id = id.toLowerCase();
-        Word_Description word_des = new Word_Description();
-        c = db.rawQuery("select * from Word where Lower(Word) Like '%" +new_id+"%'", new String[]{});
-        StringBuffer buffer = new StringBuffer();
-        int des_id = 0;
-        while(c.moveToNext()){
-            des_id = c.getInt(3);
-        }
-        c.close();
-        return des_id;
+        wordModel = new WordModel();
+        return wordModel.getWordDesIdbyName(id,c,db);
     }
 }
